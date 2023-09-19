337e4716fa4eae6cc906f07a3274f2cf5f83f259
jahangir shah <jahangirshahofficial@gmail.com>
2023-09-19 23:34:29 +0500

    initial commit

--- config/config.php
+++ config/config.php
@@ -1,2 +1,2 @@
 <?php 
-$con = mysqli_connect('localhost','root','','bank-app') or die('fail to connect with DB');
+$con = mysqli_connect("https://trnsaction-app.000webhostapp.com",'root','','bank-app') or die('fail to connect with DB');
